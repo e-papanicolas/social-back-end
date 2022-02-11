569f27832915f408cc2c0913883d0b812d1d9ed3
Eleni Papanicolas <76261375+e-papanicolas@users.noreply.github.com>
2022-02-11 10:08:56 -0500

    initial file and db setup

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :users, only: [:create] #create - signup
+  post '/login', to: 'auth#create'
+  get '/me', to: 'users#me' #profile page
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
