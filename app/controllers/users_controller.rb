cc25a97615073c12ecf3903414b5cac7fc16b49d
Eleni Papanicolas <76261375+e-papanicolas@users.noreply.github.com>
2022-02-15 16:26:55 -0500

    update user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   
   def create 
     
-    @user = User.create(user_params)
+    @user = User.create!(user_create_params)
     if @user.valid?
       @token = encode_token({user_id: @user.id})
       render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
 
   def bio_edit
     user = find_user 
-    user.update!(update_bio)
+    user.update!(user_params)
     render json: { user: UserSerializer.new(current_user) }, status: :accepted
   end
 
@@ -52,7 +52,11 @@ class UsersController < ApplicationController
     User.find_by(id: params[:id])
   end
 
-  def user_params
+  def user_params 
+    params.permit(:username, :password, :email, :first_name, :last_name, :bio, :avatar, :friends)
+  end
+
+  def user_create_params
     params.require(:user).permit(:username, :password, :email, :first_name, :last_name, :bio, :avatar, :friends)
   end
 
