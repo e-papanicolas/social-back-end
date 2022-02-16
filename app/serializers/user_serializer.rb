ef24270b65998574cdb0b45cb4934b10223d77d1
Hung-Le <theorangedot@hungs-mac-mini.lan>
2022-02-16 21:03:47 +0900

    fixed friends controller action

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :first_name, :last_name, :email, :bio, :avatar, :created_at, :friends
 
+
+  def friends
+    self.object.friends.map {|friend| User.find_by(id: friend)}
+  end
   has_many :posts
 end
